5967cb853d82443437cb79fe62175a3547808c82
KILLER7sv <116873144+KILLER7sv@users.noreply.github.com>
2023-07-04 20:13:26 +0530

    fixed

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -65,14 +65,14 @@ public class HotelManagementController {
     }
 
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
-    public List<String> getBookings() {
-        // In this function, return the bookings done by a person
+    public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
+    {
+        //In this function return the bookings done by a person
         User object = new User();
-        double aadharCard = object.getaadharCardNo();
         String naam = object.getName();
         List<String > lst = new ArrayList<>();
         lst.add(naam);
-        return lst;
+        return lst.size();
     }
 
     @PutMapping("/update-facilities")
